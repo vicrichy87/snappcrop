7271ce97111ddee22a1db29995eaae07130bd24b
vicrichy87 <62028881+vicrichy87@users.noreply.github.com>
2025-10-22 19:38:06 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,6 @@
 // pages/_app.js
 import '../styles/globals.css';
-import Navbar from '../components/Navbar';
 
 export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <Navbar />
-      <main className="pt-20"> {/* pushes content below navbar */}
-        <Component {...pageProps} />
-      </main>
-    </>
-  );
+  return <Component {...pageProps} />;
 }
