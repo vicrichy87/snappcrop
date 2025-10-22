8e00934283113374cf965bc9d10d036352c0ed9e
vicrichy87 <62028881+vicrichy87@users.noreply.github.com>
2025-10-22 19:36:46 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,10 @@
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./app/**/*.{js,ts,jsx,tsx}"
+    "./components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {
-      colors: {
-        primary: "#0070f3",
-        accent: "#6366f1",
-        sky: "#38bdf8"
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
