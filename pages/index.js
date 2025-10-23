46144262d44b8e3548657db0a6da6142dbbb0d03
vicrichy87 <62028881+vicrichy87@users.noreply.github.com>
2025-10-23 20:18:41 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ import { supabase } from "../lib/supabase";
 import { getSession } from "../lib/session";
 import Lottie from "lottie-react";
 import aiTransform from "../public/ai-transform.json";
-import Human from 'human';
+import Human from 'human'; //human api
 
 /**
  * Snappcrop - Full Feature Home Page
